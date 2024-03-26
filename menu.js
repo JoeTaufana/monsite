5bf684d4b59b08d7c4728adedc98d682f57669c6
JoeTaufana <taufanajoe@gmail.com>
2024-03-26 21:43:41 +0100

    crétions de la page Savoir faire et maj style.css

--- menu.js
+++ menu.js
@@ -22,22 +22,40 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+window.onload = () => {
+  // On va chercher tous les éléments avec la classe "accordionElement"
+  const elts = document.querySelectorAll(".accordionElement");
 
-$( function() {
-  $( "#accordion" )
-    .accordion({
-      header: "> div > h3"
-    })
-    .sortable({
-      axis: "y",
-      handle: "h3",
-      stop: function( event, ui ) {
-        // IE doesn't register the blur when sorting
-        // so trigger focusout handlers to remove .ui-state-focus
-        ui.item.children( "h3" ).triggerHandler( "focusout" );
+  for (let elt of elts) {
+    elt.addEventListener("click", function() {
+      // On récupère l'élément actif
+      const active = document.querySelector(".accordionElement.active");
+      
+      // Si l'élément cliqué n'est pas déjà actif
+      if (active !== this) {
+        // On retire la classe "active" de l'élément actif
+        if (active) {
+          active.classList.remove("active");
+          // On réduit sa hauteur
+          active.querySelector(".accordionContent").style.height = 0;
+        }
 
-        // Refresh accordion to handle new order
-        $( this ).accordion( "refresh" );
+        // On met la classe "active" à l'élément cliqué
+        this.classList.add("active");
+
+        // On récupère la balise <ul> à l'intérieur de l'élément cliqué
+        let section = this.querySelector(".accordionContent ul");
+
+        // On récupère la hauteur de la balise <ul>
+        let sectionHeight = section.offsetHeight + 25;
+
+        // On définit la hauteur de .accordionContent
+        this.querySelector(".accordionContent").style.height = sectionHeight + "px";
+      } else {
+        // Si l'élément cliqué est déjà actif, on le réduit
+        this.classList.remove("active");
+        this.querySelector(".accordionContent").style.height = 0;
       }
     });
-} );
+  }
+}
